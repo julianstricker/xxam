5e388d692cfdab58db438df21921c5888ab1b19c
Julian Stricker <julian@julianstricker.com>
2015-10-15 19:37:11 +0200

    Form types should be in Form/Type folders Removed ContactController from AdminBunde

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ umask(0000);
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
   //  || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '85.25.199.157','95.244.104.240','95.171.62.140','79.50.105.178')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '85.25.199.157','95.244.104.240','95.171.62.140','79.50.105.178','91.65.252.191')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
