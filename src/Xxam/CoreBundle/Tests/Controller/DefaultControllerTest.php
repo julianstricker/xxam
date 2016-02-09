e1234c02369a649025dbcea32f8b4773c90ab799
Julian Stricker <julian.stricker@brandnamic.com>
2016-02-09 15:50:42 +0100

    deprecation warnings

--- src/Xxam/CoreBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Xxam/CoreBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,12 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue(
+            $client->getResponse()->isRedirect()
+        );
+
+       $this->assertTrue($crawler->filter('body')->count() > 0);
     }
 }
