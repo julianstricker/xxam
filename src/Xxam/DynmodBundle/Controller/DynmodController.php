f1163b28bd59e0bdf21a62c377ead13caba69030
Julian Stricker <julian.stricker@brandnamic.com>
2016-03-02 08:41:24 +0100

    Dynmod & Loggable

--- src/Xxam/DynmodBundle/Controller/DynmodController.php
+++ src/Xxam/DynmodBundle/Controller/DynmodController.php
@@ -12,6 +12,7 @@ use Xxam\CoreBundle\Entity\LogEntryRepository;
 use Xxam\DynmodBundle\Entity\Dynmod;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Xxam\DynmodBundle\Entity\DynmodRepository;
 
 /**
  * Dynmod controller.
@@ -29,10 +30,10 @@ class DynmodController extends Controller
      *
      * @Route("", name="dynmod")
      * @Method("GET")
-     * @Template()
      * @Security("has_role('ROLE_DYNMOD_LIST')")
      */
     public function indexAction() {
+        /** @var DynmodRepository $repository */
         $repository=$this->getDoctrine()->getManager()->getRepository('XxamDynmodBundle:Dynmod');
         return $this->render('XxamDynmodBundle:Dynmod:index.js.twig', array('modelfields'=>$repository->getModelFields(),'gridcolumns'=>$repository->getGridColumns()));
     }
@@ -42,44 +43,57 @@ class DynmodController extends Controller
      *
      * @Route("/edit", name="dynmod_new")
      * @Method("GET")
-     * @Template()
      * @Security("has_role('ROLE_DYNMOD_CREATE')")
      */
     public function newAction() {
+        /** @var DynmodRepository $repository */
         $repository=$this->getDoctrine()->getManager()->getRepository('XxamDynmodBundle:Dynmod');
         $entity=new Dynmod();
         return $this->render('XxamDynmodBundle:Dynmod:edit.js.twig', array('entity'=>$entity,'modelfields'=>$repository->getModelFields()));
     }
-    
+
     /**
      * Show create form.
      *
      * @Route("/edit/{id}", name="dynmod_edit")
      * @Method("GET")
-     * @Template()
      * @Security("has_role('ROLE_DYNMOD_EDIT')")
+     * @param $id
+     * @param Request $request
+     * @return Response
      */
     public function editAction($id,Request $request) {
         $em = $this->getDoctrine()->getManager();
         $version=$request->get('version',null);
+        /** @var DynmodRepository $repository */
         $repository=$this->getDoctrine()->getManager()->getRepository('XxamDynmodBundle:Dynmod');
+        $data=[];
         if ($version){
             $entityname='Xxam\DynmodBundle\Entity\Dynmod';
             /** @var LogEntryRepository $logrepo */
             $logrepo=$em->getRepository('XxamCoreBundle:LogEntry');
             $entity= $em->find($entityname,$id);
-            //$logs = $logrepo->getLogEntries($entity);
+            $logs = $logrepo->getLogEntries($entity);
             $logrepo->revert($entity, $version);
+            foreach($logs as $log) {
+                if ($log->getVersion()==$version){
+                    $data['log']=$log;
+                    break;
+                }
+            }
+
         }else{
             $entity = $repository->find($id);
+            $data['log']=null;
         }
 
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Dynmod entity.');
         }
-
-        return $this->render('XxamDynmodBundle:Dynmod:edit.js.twig', array('entity'=>$entity,'modelfields'=>$repository->getModelFields()));
+        $data['entity']=$entity;
+        $data['modelfields']=$repository->getModelFields();
+        return $this->render('XxamDynmodBundle:Dynmod:edit.js.twig', $data);
     }
     
 
